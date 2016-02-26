0ddaf1d720b912ccff0b148ce8e3ae989fd2b62c
Dominic Monn <dm@impac.ch>
2016-02-26 15:11:34 +0100

    added play and start controllers

---
+++ WYR/PlayController.swift
@@ -0,0 +1,26 @@
+//
+//  PlayController.swift
+//  WYR
+//
+//  Created by Dominic Monn on 26.02.16.
+//  Copyright © 2016 Dominic Monn. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class PlayController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ WYR/StartController.swift
@@ -0,0 +1,26 @@
+//
+//  StartController.swift
+//  WYR
+//
+//  Created by Dominic Monn on 26.02.16.
+//  Copyright © 2016 Dominic Monn. All rights reserved.
+//
+
+import Foundation
+
+import UIKit
+
+class StartController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
